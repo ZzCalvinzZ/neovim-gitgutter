740857acb9f40216027755e97fe44a47fe88c204
Calvin Collins <calvinc@surveymonkey.com>
2015-06-13 22:59:32 -0400

    space

--- rplugin/python/neovim-gitgutter/__init__.py
+++ rplugin/python/neovim-gitgutter/__init__.py
@@ -4,7 +4,7 @@ import neovim
 class Snake(object):
 	def __init__(self, vim):
 		self.vim = vim
- 
+
 	@neovim.command('Test', sync=True)
 	def snake_start(self):
-	self.vim.current.buffer[0] = 'Hi mama'
+		self.vim.current.buffer[0] = 'Hi mama'
