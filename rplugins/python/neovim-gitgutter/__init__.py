bbc667d2f56b6e84d0b2f1a753c5e05cef37b121
Calvin Collins <calvinc@surveymonkey.com>
2015-06-13 22:30:37 -0400

    initial neovim test

---
+++ rplugins/python/neovim-gitgutter/__init__.py
@@ -0,0 +1,10 @@
+import neovim
+
+@neovim.plugin
+class Snake(object):
+	def __init__(self, vim):
+		self.vim = vim
+ 
+	@neovim.command('Test', sync=True)
+	def snake_start(self):
+	self.vim.current.buffer[0] = 'Hi mama'
